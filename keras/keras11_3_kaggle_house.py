ee3f8bd80aabb5ab2a5c7aa9eb72623155a786df
thiefdirk <balcony_rarer_0z@icloud.com>
2022-06-29 14:25:12 +0900

    14:25

--- keras/keras11_3_kaggle_house.py
+++ keras/keras11_3_kaggle_house.py
@@ -55,7 +55,7 @@ print(train_set_encoded)
 
 test_set_encoded = test_set.drop(numerical_feats_,axis=1)
 print(test_set_encoded)
-
+###################범주형변수값 수치형으로 변환###############
 le = LabelEncoder()
 
 train_set_encoded.loc[:,:] = \
@@ -66,6 +66,74 @@ print(train_set_encoded)
 train_set = pd.concat([train_set_encoded, train_set.loc[:,numerical_feats]], axis=1)
 
 print(train_set)
+
+test_set_encoded.loc[:,:] = \
+test_set_encoded.loc[:,:].apply(LabelEncoder().fit_transform)    
+
+print(test_set_encoded)
+
+test_set = pd.concat([test_set_encoded, test_set.loc[:,numerical_feats_]], axis=1)
+
+print(test_set)
+
+##############################################################
+train_set = train_set.fillna(0) # dropna() : train_set 에서 na, null 값 들어간 행 삭제
+test_set = test_set.fillna(0) # test_set 에서 이빨빠진데 바로  ffill : 위에서 가져오기 test_set.mean : 평균값
+
+
+x = train_set.drop(['SalePrice'], axis=1)
+y = train_set['SalePrice']
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    train_size=0.75,
+                                                    random_state=31
+                                                    )
+print(x_train)
+print(y_train)
+
+#2. 모델구성
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, input_shape=(79, 1), dropout=0.0, recurrent_dropout=0.2,))
+#model.add(LSTM(164, return_sequences=True, input_shape=(n_steps, n_features)))
+#model.add(LSTM(164, return_sequences=True, input_shape=(n_steps, n_features)))
+model.add(LSTM(32))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=800, batch_size=100, verbose=1)
+
+#4. 평가, 예측
+loss = model.evaluate(x, y) 
+print('loss : ', loss)
+
+y_predict = model.predict(x_test)
+
+def RMSE(a, b): 
+    return np.sqrt(mean_squared_error(a, b))
+
+rmse = RMSE(y_test, y_predict)
+print("RMSE : ", rmse)
+
+y_summit = model.predict(test_set)
+
+print(y_summit)
+print(y_summit.shape) # (715, 1)
+
+submission_set = pd.read_csv(path + 'sample_submission.csv', # + 명령어는 문자를 앞문자와 더해줌
+                             index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
+
+print(submission_set)
+
+submission_set['SalePrice'] = y_summit
+print(submission_set)
+
+
+submission_set.to_csv(path + 'submission.csv', index = True)
+
+
+# loss :  428836256.0
+# RMSE :  32729.866146587963
 
 '''
 
@@ -91,16 +159,12 @@ ohe_encoded_ = ohe.transform(categorical_feats_)
 new_cat_ = pd.DateFrame(ohe_encoded_)
 categorical_feats_ = pd.concat([categorical_feats_, new_cat_], axis=1)
 
-x = train_set.drop(['saleprice'], axis=1)
-y = train_set['saleprice']
+
 
 print(x.shape)
 print(y.shape)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.75,
-                                                    random_state=31
-                                                    )
+
 
 print(x_train)
 print(x_test)
