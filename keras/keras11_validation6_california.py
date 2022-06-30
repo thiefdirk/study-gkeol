ad4f6ce10045b32067a8537231a47854d103165e
thiefdirk <balcony_rarer_0z@icloud.com>
2022-06-30 20:35:01 +0900

    0630

--- keras/keras11_validation6_california.py
+++ keras/keras11_validation6_california.py
@@ -15,10 +15,11 @@ from sklearn.model_selection import train_test_split
 datasets = fetch_california_housing()
 x = datasets.data
 y = datasets.target
-
+print(x)
+print(y)
 x_train, x_test, y_train, y_test = train_test_split(x,y,
                                                     train_size=0.7,
-                                                    random_state=72
+                                                    random_state=18
                                                     )
 
 #2. 모델구성
@@ -28,12 +29,13 @@ model.add(Dense(20, input_dim=8))
 model.add(Dense(30))
 model.add(Dense(50))
 model.add(Dense(50))
+model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=500, batch_size=100, verbose = 1, validation_split=0.2)
+model.fit(x_train, y_train, epochs=1000, batch_size=100, verbose = 1, validation_split=0.2)
 
 #4. 평가, 예측
 loss = model.evaluate(x_test, y_test)
@@ -47,6 +49,6 @@ print('r2스코어 : ', r2)
 
 # loss :  0.6447481513023376
 # r2스코어 :  0.5096276859675669
-##################val전후#################
+##################val전후#################  72
 # loss :  0.5981025099754333
-# r2스코어 :  0.545104755121719
+# r2스코어 :  0.545104755121719   
