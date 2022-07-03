974069f99d48d5047b0aca9b1b02d5d29dd51562
thiefdirk <balcony_rarer_0z@icloud.com>
2022-07-03 17:37:59 +0900

    07.03

--- keras/keras14_2_activation_boston.py
+++ keras/keras14_2_activation_boston.py
@@ -9,10 +9,10 @@ from sklearn.model_selection import train_test_split
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 import matplotlib.pyplot as plt
-from matplotlib import font_manager, rc
-font_path = "C:/Windows/Fonts/gulim.TTc"
-font = font_manager.FontProperties(fname=font_path).get_name()
-rc('font', family=font)
+# from matplotlib import font_manager, rc
+# font_path = "C:/Windows/Fonts/gulim.TTc"
+# font = font_manager.FontProperties(fname=font_path).get_name()
+# rc('font', family=font)
 from sklearn.datasets import load_boston
 import time
 
