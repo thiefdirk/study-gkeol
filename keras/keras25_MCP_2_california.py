9d364d0371d6c8b5ec02200a2e196dce6f2b4c15
thiefdirk <balcony_rarer_0z@icloud.com>
2022-07-07 18:20:00 +0900

    폴더명 자동생성2

--- keras/keras25_MCP_2_california.py
+++ keras/keras25_MCP_2_california.py
@@ -1,9 +1,9 @@
 from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.models import Sequential, load_model
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.models import Sequential, Model, load_model
+from tensorflow.python.keras.layers import Dense, Input
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.datasets import load_boston
 import numpy as np
+from sklearn.datasets import fetch_california_housing
 import time
 
 ###########################폴더 생성시 현재 파일명으로 자동생성###########################################
@@ -15,9 +15,12 @@ print(a.split("\\")[-1]) #현재 파일 명
 current_name = a.split("\\")[-1]
 ##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감#######################
 
+
+
 #1. 데이터
-datasets = load_boston()
-x, y = datasets.data, datasets.target
+datasets = fetch_california_housing()
+x = datasets.data
+y = datasets.target
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,
                                                     train_size=0.8,
@@ -25,23 +28,41 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,
                                                     )
 
 scaler = MinMaxScaler()
-x_train = scaler.fit_transform(x_train)
+# scaler = StandardScaler()
+# scaler = MaxAbsScaler()
+# scaler = RobustScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
+print(np.min(x_train))  # 0.0
+print(np.max(x_train))  # 1.0
 
-start_time = time.time()
+print(np.min(x_test))  # 1.0
+print(np.max(x_test))  # 1.0
+
 
 #2. 모델구성
 
-model = Sequential()
-model.add(Dense(256, input_dim=13))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1))
-model.summary()
+# model = load_model("./_save/keras22_hamsu02_california.h5")
 
+# model = Sequential()
+# model.add(Dense(20, input_dim=8,activation='sigmoid'))
+# model.add(Dense(30,activation='relu'))
+# model.add(Dense(50,activation='relu'))
+# model.add(Dense(50,activation='relu'))
+# model.add(Dense(50,activation='relu'))
+# model.add(Dense(10))
+# model.add(Dense(1))
+
+input1 = Input(shape=(8,))
+dense1 = Dense(20)(input1)
+dense2 = Dense(30, activation='sigmoid')(dense1)
+dense3 = Dense(50, activation='relu')(dense2)
+dense4 = Dense(50, activation='relu')(dense3)
+dense5 = Dense(50, activation='relu')(dense4)
+dense6 = Dense(10, activation='relu')(dense5)
+output1 = Dense(1)(dense6)
+model = Model(inputs=input1, outputs=output1)
 
 #3. 컴파일, 훈련
 
@@ -58,7 +79,7 @@ print(date)
 filepath = './_ModelCheckPoint/' + current_name + '/'
 filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
 
-earlyStopping = EarlyStopping(monitor='val_loss', patience=10, mode='auto', verbose=1, 
+earlyStopping = EarlyStopping(monitor='val_loss', patience=100, mode='auto', verbose=1, 
                               restore_best_weights=True)        
 
 mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, save_best_only=True, 
@@ -70,16 +91,11 @@ hist = model.fit(x_train, y_train, epochs=1000, batch_size=100,
                  callbacks=[earlyStopping, mcp],
                  verbose=1)
 
-end_time = time.time() - start_time
-
-
 #4. 평가, 예측
-
-print("=============================1. 기본 출력=================================")
 loss = model.evaluate(x_test, y_test)
 y_predict = model.predict(x_test)
+
 from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print('loss : ' , loss)
 print('r2스코어 : ', r2)
-print("걸린시간 : ", end_time)
---
+++ keras/keras25_MCP_3_diabets.py
@@ -0,0 +1,96 @@
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.models import Sequential, Model, load_model
+from tensorflow.python.keras.layers import Dense, Input
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path = "C:/Windows/Fonts/gulim.TTc"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+from sklearn.datasets import load_diabetes
+import time
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+import numpy as np
+
+
+#1. 데이터
+datasets = load_diabetes()
+x = datasets.data
+y = datasets.target
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    train_size=0.8,
+                                                    random_state=72
+                                                    )
+
+# scaler = MinMaxScaler()
+# scaler = StandardScaler()
+scaler = MaxAbsScaler()
+# scaler = RobustScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+print(np.min(x_train))  # 0.0
+print(np.max(x_train))  # 1.0
+
+print(np.min(x_test))  # 1.0
+print(np.max(x_test))  # 1.0
+
+
+#2. 모델구성
+
+# model = load_model("./_save/keras22_hamsu03_diabets.h5")
+
+# model = Sequential()
+# model.add(Dense(200, input_dim=10))
+# model.add(Dense(300))
+# model.add(Dense(200))
+# model.add(Dense(300,activation='relu'))
+# model.add(Dense(150))
+# model.add(Dense(180))
+# model.add(Dense(1))
+
+input1 = Input(shape=(10,))
+dense1 = Dense(200)(input1)
+dense2 = Dense(300)(dense1)
+dense3 = Dense(200)(dense2)
+dense4 = Dense(300, activation='relu')(dense3)
+dense5 = Dense(150)(dense4)
+dense6 = Dense(180)(dense5)
+output1 = Dense(1)(dense6)
+model = Model(inputs=input1, outputs=output1)
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
+from tensorflow.python.keras.callbacks import EarlyStopping
+earlyStopping = EarlyStopping(monitor='val_loss', patience=100, mode='min', verbose=1, 
+                              restore_best_weights=True)
+
+hist = model.fit(x_train, y_train, epochs=2000, batch_size=100,verbose=1,validation_split=0.2, callbacks=[earlyStopping])
+print(hist)
+
+
+end_time = time.time()
+
+model.save("./_save/keras22_hamsu03_diabets.h5")
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+y_predict = model.predict(x_test)
+
+
+
+print("걸린시간 : ", end_time)
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print('loss : ' , loss)
+print('r2스코어 : ', r2)
+
+# maxabs r2스코어 :  0.6987890148011604    loss :  1988.9619140625
+
+# loss :  2132.549072265625
+# r2스코어 :  0.677043978287722
+
+# loss :  [1966.503662109375, 34.46048355102539]   
+# r2스코어 :  0.7021901376455495
