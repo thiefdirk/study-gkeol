92aa301e7aa578d3224cc20337a3b1e991fd05f5
thiefdirk <balcony_rarer_0z@icloud.com>
2022-07-28 22:28:31 +0900

    0728

--- _toy_project/char_rnn_02.py
+++ _toy_project/char_rnn_02.py
@@ -5,23 +5,29 @@ import numpy as np
 import os
 import time
 from konlpy.tag import Okt 
+import nltk
+from nltk.tokenize import word_tokenize
 # test=tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
 
 # print(test)
 okt=Okt()  
 path = 'D:\study_data\_temp/'
 
-datasets = pd.read_csv("/Users/gkeol/Documents/study-gkeol/marrage.csv")
+datasets = pd.read_csv("/Users/gkeol/Documents/study-gkeol/marrage_orgn.csv",
+                   encoding="utf-8", names=["topic", "quote"])
+
+
+datasets['topic'] = datasets['topic'].str.strip()
 
 print(f'Length of text: {len(datasets)} characters')
 
 print(datasets[:250])
 
-vocab = sorted(set(datasets))
-
-print(f'{len(vocab)} unique characters')
-
-print(okt.morphs(datasets[1]))
+train = [(quote,topic)  for index,
+         (quote,topic)  in datasets.iterrows()]
+print(okt.morphs(datasets))
 print(okt.pos(datasets))  
 print(okt.nouns(datasets))  
+allword = set(word for sentence in train
+              for word in word_tokenize(sentence[0]))
 
