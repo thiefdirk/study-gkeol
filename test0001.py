ed449b75749eac4b23212ba3d341ee724bae3eed
thiefdirk <balcony_rarer_0z@icloud.com>
2022-11-18 23:27:43 +0900

    1118

--- test0001.py
+++ test0001.py
@@ -10,3 +10,15 @@ import tqdm
 
 a = pd.DataFrame(np.random.rand(48, 1))
 b = pd.DataFrame(np.random.rand(48, 1))
+
+# 데이터프레임에서 7과 8의 배수행 index 추출
+
+index = a.index[(a.index % 7 == 0) | (a.index % 8 == 0)]
+
+# 추출한 index를 이용하여 데이터프레임에서 행 삭제
+
+a = a.drop(index)
+
+# concat np.array
+
+c = np.concatenate((a, b), axis=1)
