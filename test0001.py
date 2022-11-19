51b13b87e5b82c22bb4819c3b638ec927ed636e3
thiefdirk <balcony_rarer_0z@icloud.com>
2022-11-19 18:37:18 +0900

    1119

--- test0001.py
+++ test0001.py
@@ -10,4 +10,12 @@ import tqdm
 
 a = pd.DataFrame(np.random.rand(48, 1))
 
-def df_to_np
+# 7배수번째 데이터들 추출후 5508번째 행까지 추출
+
+b = a.iloc[::7, :] # ::7은 7의 배수번째 데이터 추출 
+
+# lstm stateful = True 모델
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.LSTM(1, batch_input_shape=(1, 7, 1), stateful=True))
+model.add(tf.keras.layers.Dense(1))
